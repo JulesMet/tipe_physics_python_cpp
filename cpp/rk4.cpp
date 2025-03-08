4a7f088f6d035d17807732359e54dbe67655ba19
Maxence <maxence.lemoine29@gmail.com>
2025-03-08 23:18:31 +0100

    corrected rk4 cpp file

--- cpp/rk4.cpp
+++ cpp/rk4.cpp
@@ -39,6 +39,7 @@ Vec2<ldouble> AttractionForce(const Object<nb_Iterations>& source, const Object<
 
 #ifndef TESTS
 int main(int argc, char** argv) {
+    std::cout << "Starting the program ... \n";
     const char* filepath = "simulation_data.log";
 
     std::ofstream file_stream(filepath, std::fstream::trunc);
@@ -48,19 +49,22 @@ int main(int argc, char** argv) {
         std::cout << "Can't open " << filepath << " !\nData have not been generated!" << std::endl;
         return EXIT_FAILURE;
     }
+    std::cout << filepath << " is open\n";
 
-    if(argc < 1 || argc > 8)
+    if(argc != 9)
     {
         // errors, there should have been more parameters send to the program
-        file_stream << "Error - Not enough data";
         std::cout << "Not enough data have been sended to the program !" << std::endl;
+        file_stream << "Error - Not enough data";
         file_stream.close();
         return EXIT_FAILURE;
     }
 
+    std::cout << "the right number of arguments have been passed (argc=" << argc << ")\n";
 
     /*
     * Argv :
+    *   0) name of the command
     *   1) Nombre de jours à simuler
     *   2) timestep in second
     *   3) Masse de l'astre fixe
@@ -72,19 +76,24 @@ int main(int argc, char** argv) {
     * 
     * */
 
+    
+    std::cout << "Initialising variables\n";
+
     char* _stopstring;
 
-    const ldouble timestep =    strtold(argv[1], &_stopstring);     // timestep of the simulation
-    const ldouble m_sun =       strtold(argv[2], &_stopstring);     // mass of the sun in kg
-    const ldouble m =           strtold(argv[3], &_stopstring);     // mass of the moving planet in kg
+    const ldouble timestep =    strtold(argv[2], &_stopstring);     // timestep of the simulation
+    const ldouble m_sun =       strtold(argv[3], &_stopstring);     // mass of the sun in kg
+    const ldouble m =           strtold(argv[4], &_stopstring);     // mass of the moving planet in kg
     const Vec2<ldouble> initial_position (
-                                strtold(argv[4], &_stopstring),     // initial x position
-                                strtold(argv[5], &_stopstring));    // initial y position
+                                strtold(argv[5], &_stopstring),     // initial x position
+                                strtold(argv[6], &_stopstring));    // initial y position
     const Vec2<ldouble> initial_speed (
-                                strtold(argv[6], &_stopstring),     // initial x speed
-                                strtold(argv[7], &_stopstring));    // initial y speed
+                                strtold(argv[7], &_stopstring),     // initial x speed
+                                strtold(argv[8], &_stopstring));    // initial y speed
     
-    const uint nbIteration = (atoi(argv[0]) * 24 * 60 * 60) / timestep;
+    const uint nbIteration = (atoi(argv[1]) * 24 * 60 * 60) / timestep;
+
+    std::cout << "All variables have been initialised" << std::endl;
 
     file_stream.close();
     return EXIT_SUCCESS;
